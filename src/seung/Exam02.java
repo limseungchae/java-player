30ec08ec6baf5c5a4e68fb5a7ae76bb9e88b1213
seungchae <tmdco12@naver.com>
2023-04-04 08:10:27 +0900

    변수,상수 사용하기

---
+++ src/seung/Exam02.java
@@ -0,0 +1,21 @@
+package seung;
+
+public class Exam02 {
+    public static void main(String[] args) {
+        System.out.println(2 + 3);
+
+        // 변수, 상수, 자료(데이터)형, 대입
+        // 남자 주번 = "홍길동";
+        int num1 = 2;
+        int num2 = 3;
+
+        // 계산 결과를 변수에 대입
+        int result = num1 + num2;
+
+        // 변수에 저장된 값을 출력
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num1 + num2);
+        System.out.println(result);
+    }
+}
