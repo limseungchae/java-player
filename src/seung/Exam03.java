eced4b1409296bfc40aed6bcf4b3a2b33df74eb0
seungchae <tmdco12@naver.com>
2023-04-04 08:10:37 +0900

    산술 연산자 사용하기

---
+++ src/seung/Exam03.java
@@ -0,0 +1,31 @@
+package seung;
+
+public class Exam03 {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 2;
+
+        int result1 = num1 + num2;
+        System.out.println(result1);
+
+        int result2 = num1 - num2;
+        System.out.println(result2);
+
+        int result3 = num1 * num2;
+        System.out.println(result3);
+
+        int result4 = num1 / num2;
+        System.out.println(result4);
+
+        int result5 = num1 % num2;
+        System.out.println(result5);
+
+        System.out.println("홍길동");
+        System.out.println(5);
+        System.out.println(num1);
+        System.out.println(num1 + num2);
+        System.out.println(2 + 3);
+        System.out.println(num1 + 2);
+        System.out.println();
+    }
+}
